2459c9d9583da7b89b7ab447c6c2203fd0301c64
agallo <andrea.gallo@jbknowledge.com>
2019-02-14 17:45:54 -0300

    Add services

--- src/App.js
+++ src/App.js
@@ -3,16 +3,32 @@ import {csv} from 'd3-request';
 import Papa from 'papaparse';
 import csvFilePath from "./data/demoCompressorWeekData.csv"
 import './App.css';
+import '../node_modules/react-vis/dist/style.css';
+import "react-datepicker/dist/react-datepicker.css";
 import _ from 'lodash';
+import DatePicker from 'react-datepicker';
+import {FlexibleWidthXYPlot , XAxis, YAxis, HorizontalGridLines, VerticalGridLines, LineSeries} from 'react-vis';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loadError: false,
-      data: null
+      data: null,
+      startDate: null,
+      endDate: null, 
+      compressorStates: {
+        // offstate no data at all, days that left to complete the week
+        unloadedMaxValue: null, // minValue
+        loadedMinValue: null, // 20% max value
+        loadedMaxValue: null
+      }
     };
     this.loadData = this.loadData.bind(this);
+    this.handleChangeStart = this.handleChangeStart.bind(this);
+    this.handleChangeEnd = this.handleChangeEnd.bind(this);
+    this.getCompressorState = this.getCompressorState.bind(this);
+    this.processData = this.processData.bind(this);
   }
 
   loadData = (data) => {
@@ -25,45 +41,152 @@ class App extends Component {
       if (error) {
         this.setState({loadError: true});
       }
-
-      if(data.length) {
+      if(data && data.length) {
         // agregar filter para q funcione en IE
         var filterData = data.filter( d => d.metricid.toUpperCase() ===  'PSUM_KW' ); 
-        var maximumValue = _.maxBy(filterData, function(o) { return o.recvalue; });
-        var minimumValue = _.minBy(filterData, function(o) { return o.recvalue; });
-        this.setState({data : filterData});
-        console.log('maximumValue', maximumValue);
-        console.log('minimumValue', minimumValue);
+        console.log('filterdata',filterData);
+
+        var maximumRecValue = _.maxBy(filterData, function(o) { return o.recvalue; });
+        var minimumRecValue = _.minBy(filterData, function(o) { return o.recvalue; });
+        var dataFilter = filterData.map(d => ({...d, x: Number(d.timestamp) , y: Number(d.recvalue)}));
+
+        this.setState({
+          data: dataFilter,
+          startDate: dataFilter[0].x,
+          endDate: dataFilter[dataFilter.length - 1].x,
+          compressorStates: {
+            unloadedMaxValue: Number(minimumRecValue.recvalue),
+            loadedMinValue: 0.2 * maximumRecValue.recvalue,
+            loadedMaxValue: Number(maximumRecValue.recvalue)
+          }
+        });
+
       }
     });
-
+    
+    var bufferStatesInfo = [];
+    var bufferAxesData = [];
+    Papa.LocalChunkSize = 1;
     Papa.parse(csvFilePath, {
       download: true,
       header: true,
-      complete: function(results, file) {
-        console.log("Parsing complete:", results.data);
+      chunk: partialResult => {
+        var filterData;
+        if (partialResult.data.length) {
+          filterData =  partialResult.data.filter( d => d.metricid && d.metricid.toUpperCase() ===  'PSUM_KW'); 
+        }
+        bufferStatesInfo = [...bufferStatesInfo, ...this.processData(filterData)];
+        var axesArray = filterData.map(d => ({...d, x: Number(d.timestamp) , y: Number(d.recvalue)}))
+        bufferAxesData = [...bufferAxesData, ...axesArray];
+      },
+      complete: function() {
+        //push off states
+        console.log("All done!");
+        console.log(bufferStatesInfo);
+        console.log(bufferAxesData);
       }
     })
   }
 
+  processData = (data) => {
+    var resultData = [];
+    var prevTime = data[0].timestamp;
+
+    _.each(data, (record) => {
+      var result = {
+        from: prevTime,
+        to: record.timestamp,
+        time: record.timestamp - prevTime
+      };
+      if (record.timestamp - prevTime > 30000) {
+        result.state = 'off';
+      } else {
+        result.state =  this.getCompressorState(record.recvalue);
+      }
+      resultData = [...resultData, result];
+      prevTime = record.timestamp;
+    });
+    return resultData;
+}
+
+  handleChangeStart = (startDate) => {
+    this.setState({
+      startDate: new Date(startDate).getTime()
+    });
+  }
+
+  handleChangeEnd = (endDate) => {
+    this.setState({
+      endDate: new Date(endDate).getTime()
+    });
+  }
+
+  /**
+   * Returns the state of the machine based on the current active power value
+   * @param activePower numeric: value from 'Psum_kw' key
+   */
+  getCompressorState = (activePower) => {
+    const {unloadedMaxValue, loadedMinValue} = this.state.compressorStates;
+    if (activePower === 0) return 'off';
+    if (activePower > 0 && activePower <= unloadedMaxValue) return 'unloaded';
+    if (activePower > unloadedMaxValue && activePower < loadedMinValue) return 'idle';
+    if (activePower >= loadedMinValue) return 'loaded';
+  }
+  
   render() {
+    const startDatePicker = new Date(this.state.startDate);
+    const endDatePicker =  new Date(this.state.endDate);
     if (this.state.loadError) {
       return <div>couldn't load file</div>;
     }
     if (!this.state.data) {
       return <div />;
     }
-    return (<div style={{
-      background: '#fff',
-      borderRadius: '3px',
-      boxShadow: '0 1 2 0 rgba(0,0,0,0.1)',
-      margin: 12,
-      padding: 24,
-      width: '350px'
-    }}>
-      <h1>Birth and death rates of selected countries</h1>
-      <h2>per 1,000 inhabitants</h2>
+    return ( <div>
+<DatePicker
+      selected={startDatePicker}
+      selectsStart
+      startDate={startDatePicker}
+      endDate={endDatePicker}
+      onChange={this.handleChangeStart}
+      maxDate={endDatePicker}
+      showDisabledMonthNavigation
+      showTimeSelect
+      timeFormat="HH:mm"
+      timeIntervals={15}
+      dateFormat="MMMM d, yyyy h:mm aa"
+      timeCaption="time"
+  />
+  
+  <DatePicker
+      selected={endDatePicker}
+      selectsEnd
+      startDate={startDatePicker}
+      endDate={endDatePicker}
+      onChange={this.handleChangeEnd}
+      minDate={startDatePicker}
+      showDisabledMonthNavigation
+      showTimeSelect
+      timeFormat="HH:mm"
+      timeIntervals={15}
+      dateFormat="MMMM d, yyyy h:mm aa"
+      timeCaption="time"
+  />
+  
+    <FlexibleWidthXYPlot
+      xType="time"
+      height={700}
+      xDomain={[this.state.startDate, this.state.endDate]}
+      >
+      <HorizontalGridLines />
+      <VerticalGridLines />
+      <XAxis title="Date Axis" />
+      <YAxis title="Active power Axis" />
+      <LineSeries
+        data={this.state.data}/>
+  </FlexibleWidthXYPlot>
     </div>
+      
     );
   }
 }
